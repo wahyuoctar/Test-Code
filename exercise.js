0ac3deaa67edaeb8af97a7a7b4510539d6076abb
wahyuoctar <wahyuoctar@gmail.com>
2022-02-15 14:16:18 +0700

    Exercise Day14

---
+++ exercise.js
@@ -0,0 +1,39 @@
+// 1. Remove vowels
+// Input: "javascript"
+// Output: "jvscrpt"
+
+// 2. Alphabet value
+// Hitung jumlah value suatu string dimana setiap huruf memiliki value
+// posisi huruf tersebut di dalam abjad. Contoh: A = 1, B = 2, Z = 26, J = 10
+// Examples
+// Input: "abc"
+// Output: 6
+
+// Input: "steve"
+// Output: 71
+
+// 3. Odd alphabet value
+// Sama seperti soal nomor 2, tetapi kali ini hanya hitung huruf yang memiliki
+// value ganjil
+
+// Input: "steve"
+// Output: 29
+
+// Input: "abc"
+// Output: 4
+
+// 4. Unique items
+// Diketahui ada 2 array of numbers, dimana 2 array tersebut memiliki beberapa
+// value/number yang sama. Tampilkan sebuah array baru yang berisi list value
+// unique (hanya berada di salah satu array saja)
+
+// Input:
+// arr1 = [1, 3, 7, 4]
+// arr2 = [1, 5, 3, 7, 0]
+// Output: [5, 4, 0]
+
+// Bonus Question
+// 5. Remove duplicates
+// Diketahui ada 2 array of numbers, dimana 2 array tersebut memiliki beberapa
+// value/number yang sama. Tampilkan sebuah array baru yang berisi list value
+// dari kedua array tersebut, tetapi sudah
